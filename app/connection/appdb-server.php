2c15751e1a7a4ba0f5816970cae63329bd623b3b
mykhel trinitaria <mykhel0003@yahoo.com>
2014-09-17 02:40:53 +0800

    gitignore

--- app/connection/appdb-server.php
+++ app/connection/appdb-server.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostname_db = "localhost";
-$database_db = "spg_db";
-$username_db = "root";
-$password_db = "joystick123";
+$database_db = "mykhel_jotform";
+$username_db = "mykhel_jotform";
+$password_db = "h.RRzW]#@q@R";
 
 $db = mysql_pconnect($hostname_db, $username_db, $password_db) or trigger_error(mysql_error(),E_USER_ERROR); 
 
